1b4cf493988f9d1a0bd999707ca34f59b8548811
AvishalomJan <avishalom14@gmail.com>
2018-10-14 11:29:35 +0300

    Some new changes in FactoryPattern strucrt

---
+++ FactoryPattern/FactoryPattern/MainProgram.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryPattern
+{
+    class MainProgram
+    {
+        static void Main()
+        {
+            IPhoneCreator iphoneFactory = new IPhoneCreator();
+            AndroidCreator androidFactory = new AndroidCreator();
+
+            IMobile mobileDevice = androidFactory.CreateMobileDevice();
+        }
+
+    }
+
+}
